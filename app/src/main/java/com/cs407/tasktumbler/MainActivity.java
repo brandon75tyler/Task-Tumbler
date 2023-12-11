031b850cd535f1c2a55e0de1097ba164d1d3b42d
brand <brandon75tyler@gmail.com>
2023-12-11 14:46:09 -0600

    Edited Shake Event

--- app/src/main/java/com/cs407/tasktumbler/MainActivity.java
+++ app/src/main/java/com/cs407/tasktumbler/MainActivity.java
@@ -121,8 +121,7 @@ public class MainActivity extends AppCompatActivity {
             float delta = mAccelCurrent - mAccelLast;
             mAccel = mAccel * 0.9f + delta;
             if(mAccel > 12) {
-                Toast.makeText(getApplicationContext(), "Shake event detected",
-                        Toast.LENGTH_SHORT).show();
+                shakeDetected();
             }
         }
 
@@ -130,6 +129,11 @@ public class MainActivity extends AppCompatActivity {
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
         }
     };
+
+    public void shakeDetected(){
+        Toast.makeText(getApplicationContext(), "Shake event detected",
+                Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onResume() {
